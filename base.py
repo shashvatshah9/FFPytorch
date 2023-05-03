439e60a50f9e4ef1e1157997d27ce786757f0f6d
shashvat shah <shashvat911@gmail.com>
2023-05-03 18:35:14 -0400

    net modules changes

--- base.py
+++ base.py
@@ -1,38 +1,44 @@
+from collections import OrderedDict, abc as container_abcs
+from itertools import chain, islice
+from typing import Iterator
 import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
+from torch.nn import Module, Linear
 from tqdm import tqdm
 from torch.optim import Adam
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
 from torch.utils.data import DataLoader
 
+from torch._jit_internal import _copy_to_script_wrapper
+
 
 def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
-
-    transform = Compose([
-        ToTensor(),
-        Normalize((0.1307,), (0.3081,)),
-        Lambda(lambda x: torch.flatten(x))])
+    transform = Compose(
+        [
+            ToTensor(),
+            Normalize((0.1307,), (0.3081,)),
+            Lambda(lambda x: torch.flatten(x)),
+        ]
+    )
 
     train_loader = DataLoader(
-        MNIST('./data/', train=True,
-              download=True,
-              transform=transform),
-        batch_size=train_batch_size, shuffle=True)
+        MNIST("./data/", train=True, download=True, transform=transform),
+        batch_size=train_batch_size,
+        shuffle=True,
+    )
 
     test_loader = DataLoader(
-        MNIST('./data/', train=False,
-              download=True,
-              transform=transform),
-        batch_size=test_batch_size, shuffle=False)
+        MNIST("./data/", train=False, download=True, transform=transform),
+        batch_size=test_batch_size,
+        shuffle=False,
+    )
 
     return train_loader, test_loader
 
 
 def overlay_y_on_x(x, y):
-    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
-    """
+    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]"""
     x_ = x.clone()
     x_[:, :10] *= 0.0
     x_[range(x.shape[0]), y] = x.max()
@@ -40,12 +46,30 @@ def overlay_y_on_x(x, y):
 
 
 class Net(torch.nn.Module):
-
     def __init__(self, dims):
         super().__init__()
         self.layers = []
         for d in range(len(dims) - 1):
             self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
+            self.add_module(str(d), Layer(dims[d], dims[d + 1]))
+
+    def __len__(self) -> int:
+        return len(self._modules)
+
+    @_copy_to_script_wrapper
+    def __dir__(self):
+        keys = super().__dir__()
+        keys = [key for key in keys if not key.isdigit()]
+        return keys
+
+    @_copy_to_script_wrapper
+    def __iter__(self) -> Iterator[Module]:
+        return iter(self._modules.values())
+
+    def forward(self, input):
+        for module in self:
+            input = module(input)
+        return input
 
     def predict(self, x):
         goodness_per_label = []
@@ -62,13 +86,12 @@ class Net(torch.nn.Module):
     def train(self, x_pos, x_neg):
         h_pos, h_neg = x_pos, x_neg
         for i, layer in enumerate(self.layers):
-            print('training layer', i, '...')
+            print("training layer", i, "...")
             h_pos, h_neg = layer.train(h_pos, h_neg)
 
 
-class Layer(nn.Linear):
-    def __init__(self, in_features, out_features,
-                 bias=True, device=None, dtype=None):
+class Layer(Linear):
+    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
         super().__init__(in_features, out_features, bias, device, dtype)
         self.relu = torch.nn.ReLU()
         self.opt = Adam(self.parameters(), lr=0.03)
@@ -77,9 +100,7 @@ class Layer(nn.Linear):
 
     def forward(self, x):
         x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
-        return self.relu(
-            torch.mm(x_direction, self.weight.T) +
-            self.bias.unsqueeze(0))
+        return self.relu(torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0))
 
     def train(self, x_pos, x_neg):
         for i in tqdm(range(self.num_epochs)):
@@ -87,9 +108,12 @@ class Layer(nn.Linear):
             g_neg = self.forward(x_neg).pow(2).mean(1)
             # The following loss pushes pos (neg) samples to
             # values larger (smaller) than the self.threshold.
-            loss = torch.log(1 + torch.exp(torch.cat([
-                -g_pos + self.threshold,
-                g_neg - self.threshold]))).mean()
+            loss = torch.log(
+                1
+                + torch.exp(
+                    torch.cat([-g_pos + self.threshold, g_neg - self.threshold])
+                )
+            ).mean()
             self.opt.zero_grad()
             # this backward just compute the derivative and hence
             # is not considered backpropagation.
@@ -97,15 +121,15 @@ class Layer(nn.Linear):
             self.opt.step()
         return self.forward(x_pos).detach(), self.forward(x_neg).detach()
 
-    
-def visualize_sample(data, name='', idx=0):
+
+def visualize_sample(data, name="", idx=0):
     reshaped = data[idx].cpu().reshape(28, 28)
-    plt.figure(figsize = (4, 4))
+    plt.figure(figsize=(4, 4))
     plt.title(name)
     plt.imshow(reshaped, cmap="gray")
     plt.show()
-    
-    
+
+
 if __name__ == "__main__":
     torch.manual_seed(1234)
     train_loader, test_loader = MNIST_loaders()
@@ -116,15 +140,15 @@ if __name__ == "__main__":
     x_pos = overlay_y_on_x(x, y)
     rnd = torch.randperm(x.size(0))
     x_neg = overlay_y_on_x(x, y[rnd])
-    
-    for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
-        visualize_sample(data, name)
-    
-    # net.train(x_pos, x_neg)
 
-    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
+    for data, name in zip([x, x_pos, x_neg], ["orig", "pos", "neg"]):
+        visualize_sample(data, name)
+
+    net.train(x_pos, x_neg)
+
+    print("train error:", 1.0 - net.predict(x).eq(y).float().mean().item())
 
     x_te, y_te = next(iter(test_loader))
     x_te, y_te = x_te.cuda(), y_te.cuda()
 
-    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
+    print("test error:", 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
