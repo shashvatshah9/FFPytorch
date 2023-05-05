96e396b408cb88e8c83b0c4328f55f1b3866b9fa
Flavjo Xhelollari <47611143+flavjoxhelollari@users.noreply.github.com>
2023-05-05 00:43:55 -0400

    Update FFLayer.py

--- FFLayer.py
+++ FFLayer.py
@@ -4,12 +4,12 @@ from tqdm import tqdm
 from torch.optim import Adam, Optimizer
 
 
-class FFLayer(Linear):
+class Layer(Linear):
     def __init__(
         self,
         in_features: int,
         out_features: int,
-        activation: Module = ReLU,
+        activation: Module = ReLU(),
         optimizer: Optimizer = None,
         threshold: float = 2.0,
         num_epochs: int = 1000,
@@ -26,35 +26,40 @@ class FFLayer(Linear):
         self.threshold = threshold
         self.num_epochs = num_epochs
 
-    def forward_pass(self, x):
+    def forward(self, x):
         x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
-        return self.activation(
-            torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0)
-        )
-
-    def forward(self, *input):
-        if self.training:
-            assert len(input) == 2, "Pass both positive and negative input"
-            x_pos, x_neg = tuple(input)
-
-            for _ in tqdm(range(self.num_epochs)):
-                g_pos = self.forward_pass(x_pos).pow(2).mean(1)
-                g_neg = self.forward_pass(x_neg).pow(2).mean(1)
-                # The following loss pushes pos (neg) samples to
-                # values larger (smaller) than the self.threshold.
-                loss = torch.log(
-                    1
-                    + torch.exp(
-                        torch.cat([-g_pos + self.threshold, g_neg - self.threshold])
-                    )
-                ).mean()
-                self.opt.zero_grad()
-                # this backward just compute the derivative and hence
-                # is not considered backpropagation.
-                loss.backward(retain_graph=True)
-                self.opt.step()
-
-            return self.forward_pass(x_pos), self.forward_pass(x_neg)
+        output = torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0)
+        if isinstance(self.activation, ReLU):
+            output = torch.square(output)
         else:
-            assert len(input) == 1, "Pass only 1 argument in eval mode"
-            return self.forward_pass(input[0])
+            output = self.activation(output)
+        return output
+
+    def train(self, x_pos, x_neg):
+        for i in tqdm(range(self.num_epochs)):
+            g_pos = self.forward(x_pos).pow(2).mean(1)
+            g_neg = self.forward(x_neg).pow(2).mean(1)
+            # The following loss pushes pos (neg) samples to
+            # values larger (smaller) than the self.threshold.
+            loss = torch.log(
+                1
+                + torch.exp(
+                    torch.cat([-g_pos + self.threshold, g_neg - self.threshold])
+                )
+            ).mean()
+            self.opt.zero_grad()
+            # this backward just computes the derivative and hence
+            # is not considered backpropagation.
+            loss.backward()
+            self.opt.step()
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
+
+    def predict(self, x):
+      with torch.no_grad():
+          x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+          output = torch.mm(x_direction, self.weight.T) + self.bias.unsqueeze(0)
+          if isinstance(self.activation, ReLU):
+              output = torch.square(output)
+          else:
+              output = self.activation(output)
+          return output.argmax(dim=1)
