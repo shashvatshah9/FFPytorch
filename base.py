b23d1d6d170398abcdfd19e1634d4a365883dbcf
Flavjo Xhelollari <47611143+flavjoxhelollari@users.noreply.github.com>
2023-05-16 14:44:24 -0400

    Update base.py

--- base.py
+++ base.py
@@ -12,7 +12,8 @@ from FFEncoding import FFEncoding
 overlay_y_on_x = FFEncoding.overlay
 
 
-def MNIST_loaders(train_batch_size=50, test_batch_size=50):
+
+def get_data_loaders(train_batch_size=50, test_batch_size=50):
     transform = Compose(
         [
             ToTensor(),
@@ -51,13 +52,12 @@ def training_loop(model, iterator, device, encoding="overlay"):
 
 
 def test_loop(model, test_loader, device):
-    print("Evaluating TEST....")
+    print("Evaluating...")
     model.eval()
     batch_error = 0
     for x, y in test_loader:
         x, y = x.to(device), y.to(device)
         batch_error += calc_error(model, x, y, device)
-        x, y = x.to("cpu"), y.to("cpu")
 
     avg_error = batch_error / len(test_loader)
     print(f"error: {avg_error}")
@@ -70,27 +70,39 @@ def train_loop(model, train_loader, device):
     for x, y in train_loader:
         x, y = x.to(device), y.to(device)
         batch_error += calc_error(model, x, y, device)
-        x, y = x.to("cpu"), y.to("cpu")
 
-    avg_error = batch_error / len(test_loader)
+    avg_error = batch_error / len(train_loader)
     print(f"error: {avg_error}")
     return avg_error
 
-
-def eval_loop(model, x, device, encoding="overlay"):
-    if encoding == "overlay":
-        goodness_per_label = []
-        for label in range(10):
-            h = overlay_y_on_x(x, label)
-            goodness = []
-            for module in model.children():
-                module.to(device)
-                h = module(h)
-                goodness += [h.pow(2).mean(1)]
-                module.to("cpu")
-            goodness_per_label += [sum(goodness).unsqueeze(1)]
-        goodness_per_label = torch.cat(goodness_per_label, 1)
-        return goodness_per_label.argmax(1)
+def eval_loop(model, input, device, batched_per_layer=False, encoding="overlay"):
+    if batched_per_layer == True:
+        if encoding == "overlay":
+            goodness_per_label = []
+            for label in range(10):
+                h = overlay_y_on_x(input, label)
+                goodness = []
+                for module in model.children():
+                    module.to(device)
+                    h = module(h)
+                    goodness += [h.pow(2).mean(1)]
+                    module.to("cpu")
+                goodness_per_label += [sum(goodness).unsqueeze(1)]
+            goodness_per_label = torch.cat(goodness_per_label, 1)
+            return goodness_per_label.argmax(1)
+    else:
+        model.to(device)
+        if encoding == "overlay":
+            goodness_per_label = []
+            for label in range(10):
+                h = overlay_y_on_x(input, label)
+                goodness = []
+                for module in model.children():
+                    h = module(h)
+                    goodness += [h.pow(2).mean(1)]
+                goodness_per_label += [sum(goodness).unsqueeze(1)]
+            goodness_per_label = torch.cat(goodness_per_label, 1)
+            return goodness_per_label.argmax(1)
 
 
 def calc_error(model, x, y, device) -> float:
@@ -100,7 +112,7 @@ def calc_error(model, x, y, device) -> float:
 
 if __name__ == "__main__":
     # Define parameters
-    EPOCHS = 10
+    EPOCHS = 40
     BATCH_SIZE = 50
     TRAIN_BATCH_SIZE = BATCH_SIZE
     TEST_BATCH_SIZE = BATCH_SIZE
@@ -108,7 +120,7 @@ if __name__ == "__main__":
     torch.manual_seed(1234)
 
     # Build train and test loaders
-    train_loader, test_loader = MNIST_loaders(
+    train_loader, test_loader = get_data_loaders(
         train_batch_size=TRAIN_BATCH_SIZE, test_batch_size=TEST_BATCH_SIZE
     )
 
@@ -150,6 +162,7 @@ if __name__ == "__main__":
         elapsed = end - start
         print(f"Completed epoch {epoch} in {elapsed} seconds")
     # Plot errors
+    plt.figure(figsize=(10, 6))  # Adjust the figsize to make the plot wider
     plt.plot(range(1, EPOCHS + 1), training_errors, label='Training Error')  # Use training_errors instead of errors
     plt.plot(range(1, EPOCHS + 1), testing_errors, label='Testing Error')  # Use testing_errors instead of errors
     plt.xlabel('Epoch')
@@ -157,6 +170,6 @@ if __name__ == "__main__":
     plt.title('Error over Epochs')
     plt.legend()
     plt.xticks(range(1, EPOCHS + 1))  # Set x-axis tick labels to 1, 2, ...
-    plt.yticks([i/10 for i in range(11)])  # Set y-axis tick labels with more detail (0.0, 0.1, 0.2, ..., 1.0)
-    plt.savefig('error_plot.png')  # Save the plot as a PNG file
+    plt.ylim(0.0, 0.13)  # Adjust the y-axis limits to zoom in on the range of errors
+    plt.savefig('error_plot_30_epochs.png')  # Save the plot as a PNG file
     plt.show()
